40cb5d65a1a20659d5453eec8d75861e9ab51f65
Camilo Molano <ing.camilo.molano@gmail.com>
2025-01-17 20:53:20 -0500

    Add Search Box

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
+
 
 
 
@@ -18,7 +20,8 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
   ],
   imports: [
     CommonModule,
-    CountriesRoutingModule
+    CountriesRoutingModule,
+    SharedModule,
   ]
 })
 export class CountriesModule { }
