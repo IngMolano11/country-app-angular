874426823698483d31361864e8355131960be7be
Camilo Molano <ing.camilo.molano@gmail.com>
2025-01-18 23:16:20 -0500

    styles for side bar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: ``
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
 
