7fcb6c8e57dec06e2da6ffa6313625027a5f82d8
Camilo Molano <ing.camilo.molano@gmail.com>
2025-01-20 19:33:19 -0500

    add local storage support for data persistence

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
-type Region = 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania'
+import { Region } from '../../interfaces/region.type';
+
 
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
   styleUrls: ['./by-region-page.component.css']
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
 
   public countries: Country[] = [];
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
   public selectedRegion?: Region;
   public isLoanding: boolean = false;
 
-    constructor( private countriesService: CountriesService) {}
+  constructor( private countriesService: CountriesService) {}
+
+    ngOnInit(): void {
+      this.countries = this.countriesService.cacheStore.byRegion.countries;
+      this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+    }
 
     searchByRegion( region: Region ): void{
       this.selectedRegion = region;
